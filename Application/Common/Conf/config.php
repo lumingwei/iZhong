96575c1df400dac473e5926e3d5cc21fe3596401
lumingwei <lumw@mama.cn>
2018-05-07 17:26:49 +0800

    完善

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
 	//'配置项'=>'配置值'
     'DB_TYPE'=>'mysql',      //数据库类型
-    'DB_HOST'=>'47.52.170.22',  //服务器地址
+    'DB_HOST'=>'176.122.166.164',  //服务器地址
     'DB_NAME'=>'sow',       //数据库名
-    'DB_USER'=>'shenge',       //数据库账号
-    'DB_PWD'=>'123456',      //数据库密码
+    'DB_USER'=>'root',       //数据库账号
+    'DB_PWD'=>'123456@!testS',      //数据库密码
     'DB_PORT'=>3306,         //数据库端口
     'DB_PREFIX'=>'sow_',      //数据库表前缀
     'DB_CHARSET'=>'utf8',    //数据库字符编码
